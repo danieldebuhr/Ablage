e7ad871e2e37d5e39e484af3d265a82fc21db326
daniel.debuhr <daniel.debuhr@caritas-altenoythe.de>
2015-11-24 12:08:49 +0100

    Anpassungen

--- desktop_ablage.py
+++ desktop_ablage.py
@@ -1,37 +1,48 @@
+# coding=ISO-8859-1
 import os
 import time
 import re
 import logging
+from random import randint
 
 
-def desktop(path):
+def moveToAblage(quelle, ziel):
 
-    logging.basicConfig(filename=path + "ablage.log", level=logging.DEBUG)
-    #logging.debug("starting script")
+    donotmove = ["OneNote-Notizbücher", "Add-in Express", "My Kindle Content", "Benutzerdefinierte Office-Vorlagen", "Diablo III", "Snagit", "Citavi 5", "Visual Studio 2015",
+                 "Adobe Acrobat XI", "Arduino", "MCS Electronics"]
 
-    workingdir = os.listdir(path)
+    logging.basicConfig(filename=ziel + "ablage.log", level=logging.DEBUG)
+
+    workingdir = os.listdir(quelle)
     for fileobject in workingdir:
-        if not os.path.isdir(path + fileobject):
+        if not os.path.isdir(quelle + fileobject):
             if not fileobject[fileobject.rfind("."):] == ".lnk" and fileobject != "ablage.log":
                 try:
-                    print(fileobject)
-                    os.rename(path + fileobject, path + "Desktop-Ablage\\" + fileobject)
+                    if os.path.exists(ziel + "\\" + fileobject):
+                        uniq = str(randint(1000, 9999)) + "_"
+                    else:
+                        uniq = ""
+
+                    os.rename(quelle + fileobject, ziel + "\\" + uniq + fileobject)
                 except FileExistsError as err:
-                    #logging.debug("error in moving file", str(fileobject), str(err))
+                    logging.debug("error in moving file " + str(fileobject) + ", " + str(err))
                     pass
         else:
-            if not fileobject == "Desktop-Ablage":
+            if not (fileobject in donotmove or fileobject[0:1] == "."):
                 try:
-                    os.rename(path + fileobject, path + "Desktop-Ablage\\" + fileobject)
+                    if os.path.exists(ziel + "\\" + fileobject):
+                        uniq = str(randint(1000, 9999)) + "_"
+                    else:
+                        uniq = ""
+                    os.rename(quelle + fileobject, ziel + "\\" + uniq + fileobject)
                 except FileExistsError as err:
-                    #logging.debug("error in moving dir", str(fileobject), str(err))
+                    logging.debug("error in moving dir" + ", " + str(fileobject) + ", " + str(err))
                     pass
 
 
-def ablage(path, testmode=False):
-
+def ablageAufraeumen(path, testmode=False):
     logging.basicConfig(filename=path + "ablage.log", level=logging.DEBUG)
-    #logging.debug("starting script")
+    logging.debug("starting script")
 
     # Dateien durchgehen
     workingdir = os.listdir(path)
@@ -44,19 +55,22 @@ def ablage(path, testmode=False):
             year = str(t[0])
             month = str(t[1]) if (len(str(t[1])) > 1) else "0" + str(t[1])
             if not os.path.isdir(path + year + "-" + month):
-                #logging.debug("create dir", path + str(year) + "-" + str(month))
+                logging.debug("create dir" + ", " + str(path) + str(year) + "-" + str(month))
                 if not testmode:
                     os.mkdir(path + year + "-" + month)
 
             if not testmode:
                 try:
-                    os.rename(path + fileobject, path + str(year) + "-" + str(month) + "\\" + fileobject)
+                    if os.path.exists(path + str(year) + "-" + str(month) + "\\" + fileobject):
+                        uniq = str(randint(1000, 9999)) + "_"
+                    else:
+                        uniq = ""
+                    os.rename(path + fileobject, path + str(year) + "-" + str(month) + "\\" + uniq + fileobject)
                 except FileExistsError as err:
-                    #logging.debug("error in moving file", str(fileobject), str(err))
+                    logging.debug("error in moving file" + ", " + str(fileobject) + ", " + str(err))
                     pass
 
-
-            #logging.debug("moved file to folder", str(fileobject), str(year) + "-" + str(month))
+            logging.debug("moved file to folder" + ", " + str(fileobject) + ", " + str(year) + "-" + str(month))
 
     # Ordner durchgehen
     workingdir = os.listdir(path)
@@ -66,23 +80,28 @@ def ablage(path, testmode=False):
         # Ordner
         if os.path.isdir(path + fileobject):
             # Nur Ordner, die nicht Datumskonform sind
-            if not re.search(r"^[0-9]{4}\-[0-9]{2}", str(fileobject)) and not re.search(r"^[0-9]{4}", str(fileobject)) and not \
+            if not re.search(r"^[0-9]{4}\-[0-9]{2}", str(fileobject)) and not re.search(r"^[0-9]{4}",
+                                                                                        str(fileobject)) and not \
                             fileobject[0] == "!":
                 t = time.gmtime(os.path.getmtime(path + fileobject))
                 year = str(t[0])
                 month = str(t[1]) if (len(str(t[1])) > 1) else "0" + str(t[1])
                 if not os.path.isdir(path + year + "-" + month):
-                    #logging.debug("create dir", path + str(year) + "-" + str(month))
+                    logging.debug("create dir" + ", " + str(path) + str(year) + "-" + str(month))
                     if not testmode:
                         os.mkdir(path + year + "-" + month)
 
                 if not testmode:
                     try:
-                        os.rename(path + fileobject, path + year + "-" + month + "\\" + fileobject)
+                        if os.path.exists(path + year + "-" + month + "\\" + fileobject):
+                            uniq = str(randint(1000, 9999)) + "_"
+                        else:
+                            uniq = ""
+                        os.rename(path + fileobject, path + year + "-" + month + "\\" + uniq + fileobject)
                     except FileExistsError as err:
-                        #logging.debug("error in moving folder", str(fileobject), str(err))
+                        logging.debug("error in moving folder" + ", " + str(fileobject) + ", " + str(err))
                         pass
-                #logging.debug("moved folder to folder", str(fileobject), str(year) + "-" + str(month))
+                        logging.debug("moved folder to folder" + ", " + str(fileobject) + ", " + str(year) + "-" + str(month))
 
     # Gruppen bilden
     workingdir = os.listdir(path)
@@ -96,38 +115,45 @@ def ablage(path, testmode=False):
             if re.search(r"^[0-9]{4}\-[0-9]{2}", str(fileobject)) and fileobject[0:4] != time.strftime("%Y"):
 
                 if not os.path.isdir(path + fileobject[0:4]):
-                    #logging.debug("create dir", path + fileobject[0:4])
+                    logging.debug("create dir" + ", " + str(path) + str(fileobject)[0:4])
                     if not testmode:
                         os.mkdir(path + fileobject[0:4])
 
                 # Zielordner wurde schon mal angelegt...
                 if os.path.isdir(path + fileobject[0:4] + "\\" + fileobject):
 
-                    #logging.debug("moved files of folder to folder", str(fileobject), str(fileobject)[0:4])
+                    logging.debug("moved files of folder to folder" + ", " + str(fileobject) + ", " + str(fileobject)[0:4])
                     workingsubdir = os.listdir(path + fileobject)
                     for fileObject2 in workingsubdir:
                         if not testmode:
                             try:
+                                if os.path.exists(path + fileobject[0:4] + "\\" + fileobject + "\\" + fileObject2):
+                                    uniq = str(randint(1000, 9999)) + "_"
+                                else:
+                                    uniq = ""
                                 os.rename(path + fileobject + "\\" + fileObject2,
-                                      path + fileobject[0:4] + "\\" + fileobject + "\\" + fileObject2)
+                                          path + fileobject[0:4] + "\\" + fileobject + "\\" + uniq + fileObject2)
                             except FileExistsError as err:
-                                #logging.debug("error in moving file", str(fileobject), str(err))
+                                logging.debug("error in moving file" + ", " + str(fileobject), str(err))
                                 pass
                     if not testmode:
                         os.removedirs(path + fileobject)
 
                 else:
-                    #logging.debug("moved folder to folder", str(fileobject), str(fileobject)[0:4])
+                    logging.debug("moved folder to folder" + ", " + str(fileobject) + ", " + str(fileobject)[0:4])
                     if not testmode:
                         try:
-                            os.rename(path + fileobject, path + fileobject[0:4] + "\\" + fileobject)
+                            if os.path.exists(path + fileobject[0:4] + "\\" + fileobject):
+                                uniq = str(randint(1000, 9999)) + "_"
+                            else:
+                                uniq = ""
+                            os.rename(path + fileobject, path + fileobject[0:4] + "\\" + uniq + fileobject)
                         except FileExistsError as err:
-                            #logging.debug("error in moving folder", str(fileobject), str(err))
+                            logging.debug("error in moving folder" + ", " + str(fileobject) + ", " + str(err))
                             pass
 
 
-    #logging.debug("finishing script")
-
-
-desktop("D:\\Daten\\Desktop\\")
-ablage("D:\\Daten\\Desktop\\Desktop-Ablage\\")
+moveToAblage("D:\\Eigene Dateien\\Desktop\\", "D:\\Ablage\\")
+moveToAblage("D:\\Eigene Dateien\\Downloads\\", "D:\\Ablage\\")
+moveToAblage("D:\\Eigene Dateien\\Eigene Dokumente\\", "D:\\Ablage\\")
+ablageAufraeumen("D:\\Ablage\\")
